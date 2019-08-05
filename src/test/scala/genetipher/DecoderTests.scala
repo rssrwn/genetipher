1b7bfe72b0d668a3b417d7d49333dcdbb8ecbb2a
ri616 <rssrwn@gmail.com>
2019-08-06 00:00:21 +0100

    Add tests for randomPop and randCharSwapMutation

--- src/test/scala/genetipher/DecoderTests.scala
+++ src/test/scala/genetipher/DecoderTests.scala
@@ -1,5 +1,7 @@
 package genetipher
 
+import geneticsearch.Types.{MutationFunc, Population}
+import geneticsearch.genotype.Sequence
 import org.scalatest.FunSuite
 
 import scala.util.Random
@@ -15,6 +17,47 @@ class DecoderTests extends FunSuite {
         assertResult(plaintext)(decodedOpt.get.head)
     }
 
+    test("randomPop returns a pop of expected length") {
+        val testDecoder = new DecoderTest()
+        val popSize = 100
+        val pop = testDecoder.randomPop(popSize)
+
+        assertResult(popSize)(pop.length)
+    }
+
+    test("randomPop contains no duplicate chars") {
+        val testDecoder = new DecoderTest()
+        val pop = testDecoder.randomPop(100)
+
+        val expectedDistinctLength = 26
+
+        for (genotype <- pop) {
+            assertResult(expectedDistinctLength)(genotype.distinct.length)
+        }
+    }
+
+    test("randCharSwapMutation swaps the correct number of elements") {
+        val testDecoder = new DecoderTest()
+        val numSwaps = 2
+        val mutFunc = testDecoder.randCharSwapMutation(numSwaps)
+
+        val genotype = new Sequence[Char](allChars.toSeq, null, null)
+
+        val mutated = mutFunc(genotype)
+
+        assert(genotype.length == mutated.length)
+
+        val numChanged = genotype.zip(mutated).map{ case (original, mut) =>
+            if (original == mut) {
+                0
+            } else {
+                1
+            }
+        }.sum
+
+        assertResult(numSwaps * 2)(numChanged)
+    }
+
     private def encode(text: String): String = {
         val charMap = text.foldLeft(Map[Char, Char](' ' -> ' ')) { (charMap, char) =>
             if (charMap.contains(char)) {
@@ -28,7 +71,10 @@ class DecoderTests extends FunSuite {
             }
         }
 
-        text.map(char => charMap(char))
+        val decoder = charMap.map(_.swap)
+
+        val ciphertext = text.map(char => charMap(char))
+        ciphertext
     }
 
     private def randCharFromSet(chars: Set[Char]): Char = {
@@ -39,6 +85,11 @@ class DecoderTests extends FunSuite {
 
     private val allChars: Set[Char] = {
         (Util.ASCII_LOWER_A to Util.ASCII_LOWER_Z).map(i => i.toChar).toSet
+    }
+
+    private class DecoderTest() extends Decoder("", None) {
+        override def randomPop(popSize: Int): Population[Char] = super.randomPop(popSize)
+        override def randCharSwapMutation(numSwaps: Int): MutationFunc[Sequence[Char]] = super.randCharSwapMutation(numSwaps)
     }
 
 }
