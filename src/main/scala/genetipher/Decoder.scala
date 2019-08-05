133d76009265235ae84b36e9b0e479c350afa539
ri616 <rssrwn@gmail.com>
2019-08-06 00:01:21 +0100

    Create randCharSwapMutation mutation func

--- src/main/scala/genetipher/Decoder.scala
+++ src/main/scala/genetipher/Decoder.scala
@@ -10,9 +10,9 @@ import scala.util.Random
 class Decoder(ciphertext: String, encoding: Option[String]) {
 
     // Parameters for the genetic algorithm
-    private val lamdba = 10
-    private val mu = 50
-    private val charMutationProb = 0.1f
+    private val lamdba = 20
+    private val mu = 80
+    private val numCharSwaps = 10
 
     def decode(): Option[Seq[String]] = {
         if (encoding.isEmpty) {
@@ -34,30 +34,36 @@ class Decoder(ciphertext: String, encoding: Option[String]) {
                 val bestEncodings = algorithm.get.run(randomPop(lamdba + mu))
                 val possiblePlaintexts = bestEncodings.map(enc => Util.buildPlaintext(ciphertext, enc.elems))
 
+                println("score " + EnglishScore.score(possiblePlaintexts.head))
+
                 Some(possiblePlaintexts)
             }
         }
     }
 
-    private def randomPop(popSize: Int): Population[Char] = {
-        val mutFunc = randCharMutation(charMutationProb)
+    protected def randomPop(popSize: Int): Population[Char] = {
+        val mutFunc = randCharSwapMutation(numCharSwaps)
         val pop = for (_ <- 0 until popSize) yield {
-            val elems = Util.randCharSeq(Util.NUM_LETTERS)
+            val elems = Util.randDistinctCharSeq()
             new Sequence[Char](elems, mutFunc, null)
         }
         pop.toVector
     }
 
-    private def randCharMutation(mutProb: Float): MutationFunc[Sequence[Char]] = {
+    protected def randCharSwapMutation(numSwaps: Int): MutationFunc[Sequence[Char]] = {
         genotype => {
-            val elems = genotype.map { char =>
-                val rand = Random.nextFloat()
-                if (rand <= mutProb) {
-                    Util.randChar()
-                } else {
-                    char
-                }
+            val elems = (0 until numSwaps).foldLeft(genotype.elems) { (curElems, _) =>
+                val length = curElems.length
+
+                val idx1 = Random.nextInt(length)
+                val idx2 = Random.nextInt(length)
+
+                val elem1 = curElems(idx1)
+                val elem2 = curElems(idx2)
+
+                curElems.updated(idx1, elem2).updated(idx2, elem1)
             }
+
             genotype.withElems(elems)
         }
     }
