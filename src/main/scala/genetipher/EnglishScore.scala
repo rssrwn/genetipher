bb74daa69c1f6e832585c9c968e884d865dec820
ri616 <rssrwn@gmail.com>
2019-08-10 20:10:04 +0100

    Add avgNgramScore function for computing a scaled score which can be used for completion op

--- src/main/scala/genetipher/EnglishScore.scala
+++ src/main/scala/genetipher/EnglishScore.scala
@@ -9,7 +9,7 @@ object EnglishScore {
 
     private val NGRAM_LENGTH = 3
 
-    private val (totalCount: Double, scoreMap: Map[String, Double]) = {
+    private val (highestCount: Double, meanProb: Double, scoreMap: Map[String, Double]) = {
         println(s"Loading scores from ngram file: $TRIGRAM_FILE")
 
         val bufferedSource = Source.fromFile(TRIGRAM_FILE)
@@ -20,45 +20,51 @@ object EnglishScore {
             (ngram, count)
         }.toSeq
 
-        val ngramCount = pairs.map(_._2).sum[Long].toDouble
+        //val ngramCount = pairs.map(_._2).sum[Long].toDouble
+
+        val counts = pairs.map(_._2)
+        val bestCount = counts.max.toDouble
+        val meanProb = (counts.sum.toDouble / counts.length) / bestCount
 
         val scores = pairs.map { case(ngram, count) =>
-            (ngram, count / ngramCount)
+            (ngram, math.log(count / bestCount))
         }.toMap
-
-        val sorted = scores.toSeq.sortWith(_._2 > _._2)
 
         println("Successfully loaded counts into the score map")
 
         bufferedSource.close()
 
-        (ngramCount, scores)
+        (bestCount, meanProb, scores)
     }
 
     // TODO assume no spaces, get better training data
     def score(text: String): Double = {
-//        val words = text.split(" ").toSeq
-//        words.map { word =>
-//            word.sliding(NGRAM_LENGTH).map { strSeq =>
-//                val str = strSeq.mkString.toUpperCase
-//                if (scoreMap.contains(str)) {
-//                    scoreMap(str)
-//                } else {
-//                    // Give ngrams with 0 occurrences a lower score than 1 occurrence
-//                    math.log(0.01 / totalCount)
-//                }
-//            }.sum
-//        }.sum
-
         text.filter(Util.isLetter).sliding(NGRAM_LENGTH).map { strSeq =>
             val str = strSeq.mkString.toUpperCase
             if (scoreMap.contains(str)) {
                 scoreMap(str)
             } else {
                 // Give ngrams with 0 occurrences a lower score than 1 occurrence
-                0.01 / totalCount
+                math.log(0.01 / highestCount)
             }
         }.sum
+    }
+
+    def avgNgramScore(text: String): Double = {
+        val ngrams = text.filter(Util.isLetter).sliding(NGRAM_LENGTH).toSeq
+        val totalScore = ngrams.map { strSeq =>
+            val str = strSeq.mkString.toUpperCase
+            if (scoreMap.contains(str)) {
+                val score = scoreMap(str) - math.log(meanProb)
+                score
+            } else {
+                // Give ngrams with 0 occurrences a lower score than 1 occurrence
+                val score = math.log((0.01 / highestCount) / meanProb)
+                score
+            }
+        }.sum
+
+        totalScore / ngrams.length
     }
 
 }
--- src/test/scala/genetipher/EnglishScoreTests.scala
+++ src/test/scala/genetipher/EnglishScoreTests.scala
@@ -5,12 +5,18 @@ import org.scalatest.FunSuite
 
 class EnglishScoreTests extends FunSuite {
 
-    // TODO
-    test("score function gives high scores for english sentences") {
+    test("avgNgramScore function gives high scores for english sentences") {
         val sentence = "the dog barked"
-        val score = EnglishScore.score(sentence)
+        val score = EnglishScore.avgNgramScore(sentence)
 
-        assert(score > -50)
+        assert(score > 1.0)
+    }
+
+    test("avgNgramScore gives low scores for non-english sentences") {
+        val sentence = "tkb yxs rdalop"
+        val score = EnglishScore.avgNgramScore(sentence)
+
+        assert(score < -1.0)
     }
 
 }
